e497f3a4921670ee200d2b515ccce1a263268f67
SpoddyCoder <paul@spoddycoder.com>
2021-06-19 19:23:50 +0100

    fix linting error

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const path = require( 'path' );
-const common = require( './webpack.common' );
 const { merge } = require( 'webpack-merge' );
+const common = require( './webpack.common' );
 
 module.exports = merge( common, {
     mode: 'development',
